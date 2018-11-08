5dee53c72050bba1d0f44be22cfc79d068666794
Mike Filicetti <filicetti.michael@gmail.com>
2018-11-08 15:12:15 -0800

    added prevent default feature.

--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -25,7 +25,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
-
+  event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
